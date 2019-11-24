2a84b343635e55e7f6a76f13a11f25ec9dc7e174
YorgunKelebek <56796626+YorgunKelebek@users.noreply.github.com>
2019-11-24 00:22:09 +0000

    Implemented basic 'Read more' functionality for full blog text reveal
    Implemented suggestion from pull request 'Implemented loading of more blog items'
    - Move call for initiateBlogSummaryList() to DOMContentLoaded
    - API error capture

--- js/app.js
+++ js/app.js
@@ -7,14 +7,27 @@ const pageSize = 10;
 const itemsParams = "?includeTotalCount={0}&limit={1}&order=elements.{2}";
 
 const blogSummaryTemplate = document.querySelector("#blog_summary_template");
+const apiErrorMessageTemplate = document.querySelector("#api_error_message");
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    initiateBlogSummaryList();
+});
 
 
 async function fetchKontent(api)
 {
-    const res = await fetch(api);
-    const json = await res.json();
-    return json;
+    var resError = { items: [], error: true };
+    try {
+        const res = await fetch(api);
+        const json = await res.json();
+        if (json.error_code !== undefined) { return resError; }
+        return json;
+    } catch (error) {
+        return resError;
+    }
 }
+
 
 async function initiateBlogSummaryList()
 {
@@ -22,28 +35,8 @@ async function initiateBlogSummaryList()
 	const api = apiItems.format([projectId, params]);
 	const json = await fetchKontent(api);
 	console.log(json);
-	
-	var blogSummary = "";
-	for (var i = 0; i < json.pagination.count; i++)
-	{
-		if (json.items[i].system.type === "blog_post")
-		{
-			blogSummary = buildBlogSummary(json.items[i], json.modular_content);
-			document.querySelector("main").appendChild(blogSummary);
-		}
-    }
-    setNextBatchLoad(json.pagination.next_page);
-}
 
-
-async function loadNextBlogBatch() {
-    var btnLoadMore = document.getElementById("btnLoadMore");
-    const api = btnLoadMore.dataset.next_page;
-    const json = await fetchKontent(api);
-
-    if (json.pagination.count > 0)
-    {
-        btnLoadMore.dataset.first_item = json.items[0].system.id;
+    if (json.items.length > 0) {
         var blogSummary = "";
         for (var i = 0; i < json.pagination.count; i++) {
             if (json.items[i].system.type === "blog_post") {
@@ -52,7 +45,35 @@ async function loadNextBlogBatch() {
             }
         }
         setNextBatchLoad(json.pagination.next_page);
-        scrollToNewBatch(btnLoadMore.dataset.first_item);
+    } else {
+        errorMessage = getApiError();
+        document.getElementById("btnLoadMore").setAttribute("style", "display:none;");
+        document.querySelector("main").appendChild(errorMessage);
+    }
+}
+
+
+async function loadNextBlogBatch() {
+    var btnLoadMore = document.getElementById("btnLoadMore");
+    const api = btnLoadMore.dataset.next_page;
+    const json = await fetchKontent(api);
+
+    if (json.items.length > 0) {
+        if (json.pagination.count > 0) {
+            btnLoadMore.dataset.first_item = json.items[0].system.id;
+            var blogSummary = "";
+            for (var i = 0; i < json.pagination.count; i++) {
+                if (json.items[i].system.type === "blog_post") {
+                    blogSummary = buildBlogSummary(json.items[i], json.modular_content);
+                    document.querySelector("main").appendChild(blogSummary);
+                }
+            }
+            setNextBatchLoad(json.pagination.next_page);
+            scrollToItem(btnLoadMore.dataset.first_item);
+        }
+    } else {
+        errorMessage = getApiError();
+        document.querySelector("main").appendChild(errorMessage);
     }
 }
 
@@ -65,7 +86,8 @@ function setNextBatchLoad(nextBatchUrl) {
     } else { btnLoadMore.style.display = "none"; }
 }
 
-function scrollToNewBatch(itemId) {
+
+function scrollToItem(itemId) {
     var item = document.querySelector('div[data-item_id="' + itemId + '"]');
     item.scrollIntoView();
 }
@@ -76,6 +98,7 @@ function buildBlogSummary(blog, modularContent)
     const blogSummary = document.importNode(blogSummaryTemplate.content, true);
     blogSummaryTitle = blogSummary.querySelector(".summary-title").textContent = blog.elements.title.value;
     blogSummary.querySelector(".summary-blog-post").dataset.item_id = blog.system.id;
+    blogSummary.querySelector(".blog-preview-toggle").dataset.item_id = blog.system.id;
 	if (blog.elements.blog_media___image.value.length > 0)
 	{
 		blogSummary.querySelector(".blog-image").src = blog.elements.blog_media___image.value[0].url;
@@ -83,9 +106,20 @@ function buildBlogSummary(blog, modularContent)
     blogSummary.querySelector(".summary-author").textContent = getAuthor(blog.elements.author.value[0], modularContent);
     var blogDate = new Date(blog.elements.post_date.value);
     blogSummary.querySelector(".summary-date").textContent = blogDate.toDateString();
-    blogSummary.querySelector(".summary-body").innerHTML = blog.elements.body.value.substr(0, blog.elements.body.value.indexOf("</p>"));
+    blogSummary.querySelector(".summary-body").innerHTML = blog.elements.body.value;
 	processMediaContent(blogSummary, modularContent);
 	return blogSummary;
+}
+
+
+function toggleBlogPreview(el)
+{
+    itemId = el.dataset.item_id;
+    var itemContainer = document.querySelector('div[data-item_id="' + itemId + '"]');
+    var itemSummary = itemContainer.querySelector(".summary-body");
+    itemSummary.classList.toggle("blog-preview");
+    el.innerHTML = itemSummary.classList.contains("blog-preview") ? "Read more" : "Close";
+    scrollToItem(itemId);
 }
 
 
@@ -109,18 +143,37 @@ function processMediaContent(blogSummary, data)
     {
         if (data[key].system.type === "media_embed")
         {
-            var obj = blogSummary.querySelector("[data-codename='" + key + "']");
-            if (obj !== null)
+            var objMediaEmbed = blogSummary.querySelector("[data-codename='" + key + "']");
+            if (objMediaEmbed !== null)
             {
                 var elementMediaEmbed = document.createElement('div');
                 elementMediaEmbed.innerHTML = data[key].elements.media_item__text.value;
-                obj.parentNode.replaceChild(elementMediaEmbed, obj);
+                objMediaEmbed.parentNode.replaceChild(elementMediaEmbed, objMediaEmbed);
+            }
+        }
+        if (data[key].system.type === "code_snippet")
+        {
+            var objCodeSnippet = blogSummary.querySelector("[data-codename='" + key + "']");
+            if (objCodeSnippet !== null) {
+                var elementCodeSnippet = document.createElement('pre');
+                if (data[key].elements.language.value[0].codename === "html")
+                {
+                    elementCodeSnippet.innerHTML = htmlentities.encode(data[key].elements.code.value);
+                } else
+                {
+                    elementCodeSnippet.innerHTML = data[key].elements.code.value;
+                }
+                objCodeSnippet.parentNode.replaceChild(elementCodeSnippet, objCodeSnippet);
             }
         }
     }
 	}
 }
 
+
+function getApiError() {
+    return document.importNode(apiErrorMessageTemplate.content, true);
+}
 
 
 
@@ -144,3 +197,26 @@ String.prototype.format = function (args) {
 };
 String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 /* /String format function */
+
+
+/* Encode/Decode html */
+/* https://ourcodeworld.com/articles/read/188/encode-and-decode-html-entities-using-pure-javascript */
+(function (window) {
+    window.htmlentities = {
+        encode: function (str) {
+            var buf = [];
+
+            for (var i = str.length - 1; i >= 0; i--) {
+                buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+            }
+
+            return buf.join('');
+        },
+        decode: function (str) {
+            return str.replace(/&#(\d+);/g, function (match, dec) {
+                return String.fromCharCode(dec);
+            });
+        }
+    };
+})(window);
+/* /Encode/Decode html */
