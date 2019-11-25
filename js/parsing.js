b979da5c11996b46027685e8193cdd66f2147d58
Andrew Gibson <andrew@goofballLogic.com>
2019-11-25 15:24:11 +0000

    documentation

--- js/parsing.js
+++ js/parsing.js
@@ -6,6 +6,13 @@ function extract(source, extractor) {
     return source[extractor];
 }
 
+/**
+ * Extracts the first value from a Kontent item, first by extracting a property, then (if defined) by extracting a value from the found object. Finally, returns defaultValue if the result is still undefined.
+ * @param {object} item
+ * @param {string|function} propertyExtractor - factory function taking the item as its sole parameter, or the key of the property
+ * @param {string|function} [valueExtractor] - factory function taking the property's value as its sole parameter, or the key of the value. If not specified, the property's entire value will be returned.
+ * @param {*} [defaultValue] - default value to return if the output would otherwise by undefined
+ */
 export function firstOrDefaultValue(item, propertyExtractor, valueExtractor, defaultValue) {
     // no item?
     if (!item) return defaultValue;
@@ -25,6 +32,14 @@ export function firstOrDefaultValue(item, propertyExtractor, valueExtractor, def
 	return value;
 }
 
+/**
+ * Given an item and associated property and value extractors (see firstOrDefaultValue for details), extracts the key for a piece of content, then extracts and returns the matching content object. Returns defaultContent if it would otherwise return undefined.
+ * @param {object} item
+ * @param {object} modularContent
+ * @param {function|string} propertyExtractor
+ * @param {function|string} valueExtractor
+ * @param {*} defaultContent
+ */
 export function firstOrDefaultContent(item, modularContent, propertyExtractor, valueExtractor, defaultContent) {
     // get the content's key from the item via normal property/value extraction
     const itemValue = firstOrDefaultValue(item, propertyExtractor, valueExtractor);
@@ -32,5 +47,5 @@ export function firstOrDefaultContent(item, modularContent, propertyExtractor, v
     if (!modularContent) return defaultContent;
     // extract the content
     const content = modularContent[itemValue];
-    return content || defaultContent;
+    return typeof content === "undefined" ? defaultContent : content;
 }
