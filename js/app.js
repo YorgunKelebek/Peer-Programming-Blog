467cf799e6bb70a2eca84768f0634ac43a157808
Andrew Stewart Gibson <andrew@goofballLogic.com>
2020-02-18 21:13:57 +0000

    starting node artist development (#24)
    * starting node artist development
    
    * templates can have items mapped to a template

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 import { pageOptions } from "./app/page-options.js";
 import { prepareData } from "./app/prepare-data.js";
 import { DOMContentLoaded } from "./lib/DOM.js";
+import NodeArtist from "./lib/node-artist.js";
 
 (async function main() {
 
     const preparingData = prepareData(pageOptions);
     await DOMContentLoaded();
     const { blogs, paging } = await preparingData;
+    const blogListItemArtist = NodeArtist("#blog-list-item");
+
+    const blogList = document.querySelector("ol.blogs");
+    for(let blog of blogs) {
+        const listItem = blogListItemArtist(blog);
+        blogList.appendChild(listItem);
+    }
 
 }());
