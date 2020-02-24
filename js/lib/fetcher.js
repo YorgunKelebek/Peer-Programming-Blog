dcbe729a4eef41b810072b35b906599b4bcfc0f1
Andrew Gibson <andrew@goofballLogic.com>
2020-02-24 13:20:32 +0000

    undo doctoring

--- js/lib/fetcher.js
+++ js/lib/fetcher.js
@@ -5,14 +5,3 @@ export default async function fetcher(url, parseAction = defaultResponseParser) 
     if (!fetched.ok) throw new Error(`${fetched.statusText} (${fetched.status})`);
     return await parseAction(fetched);
 }
-
-function doctor(data) {
-    const when = new Date();
-    when.setMinutes(when.getMinutes() - 30);
-    data.items[0].elements.post_date.value = when.toString();
-    when.setMinutes(when.getMinutes() - 60);
-    data.items[1].elements.post_date.value = when.toString();
-    when.setHours(when.getHours() - 24);
-    data.items[2].elements.post_date.value = when.toString();
-    return data;
-}
