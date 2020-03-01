b6ec44d0a2e9912298d32b359944b61c9a869a49
Andrew Gibson <andrew@goofballLogic.com>
2020-03-01 19:01:39 +0000

    fixing the blog list rendering

--- js/app/render-model/blogs.js
+++ js/app/render-model/blogs.js
@@ -1,15 +1,11 @@
 import NodeArtist from "../../lib/node-artist/index.js";
 
-export default function({ blogs, paging }) {
+export default function(viewModel) {
 
-    if (!blogs) return;
+    if (!viewModel.blogs) return;
 
-    const blogListItemArtist = NodeArtist("#blog-list-item");
-
-    const blogList = document.querySelector("ol.blogs");
-    for(let blog of blogs) {
-        const listItem = blogListItemArtist(blog);
-        blogList.appendChild(listItem);
-    }
+    const blogListArtist = NodeArtist("#blog-list");
+    const blogs = blogListArtist(viewModel);
+    document.body.appendChild(blogs);
 
 }
