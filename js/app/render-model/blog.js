ddd3fcee7e95fc021aa7657c2385c5faf7d8f52f
Andrew Stewart Gibson <andrew@goofballLogic.com>
2020-03-08 22:17:34 +0000

    Data preparation and rendering logic nicely separated out into modules (#42)
    * starting node artist development
    
    * templates can have items mapped to a template
    
    * mapping of arrays of data into templates is now possible
    
    * completion of blog listing
    
    * undo doctoring
    
    * refactored preparation and rendering logic out of the main app script
    
    * added names to default functions
    
    * moar names
    
    * remove redundant console.log

---
+++ js/app/render-model/blog.js
@@ -0,0 +1,5 @@
+export default function renderBlog({}) {
+
+    // not yet implemented
+
+}
---
+++ js/app/render-model/blogs.js
@@ -0,0 +1,15 @@
+import NodeArtist from "../../lib/node-artist/index.js";
+
+export default function renderBlogs({ blogs, paging }) {
+
+    if (!blogs) return;
+
+    const blogListItemArtist = NodeArtist("#blog-list-item");
+
+    const blogList = document.querySelector("ol.blogs");
+    for(let blog of blogs) {
+        const listItem = blogListItemArtist(blog);
+        blogList.appendChild(listItem);
+    }
+
+}
